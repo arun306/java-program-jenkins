a376c6ac903a2327cbe792eb484e6cdd48fa7ad4
Arun Sabba <sabbaarun.639@gmail.com>
2024-08-05 23:09:21 +0530

    changes #2

--- Main.java
+++ Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is output from java program");
         System.out.println(new Date());
-        throw new RuntimeException();
     }
 }
